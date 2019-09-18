a115a845509d2709c2065d4715fe932fa4a346cd
Miguel Mena Correa <miguelmena@outlook.com>
2019-09-18 15:56:55 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 app.get('/', function(req, res) {
-    res.send('Hola mundo desde el sena chasdfasdfasdfinazo');
+    res.send('Hola mundo desde el sena');
 });
 app.listen(3000, function() {
     console.log(`Server running at port ${PORT}`);
